cf180f17182a81e1f8663ea37a55d8a156ec3bec
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-19 11:08:19 +0300

    feat: implement activity of buttons

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,15 +1,40 @@
 import { Container } from "../Container";
 import { Button } from "../Button";
+import type { TMode } from "../../App";
 
 import styled from "./Controls.module.scss";
 
-export const Controls = (): JSX.Element => {
+type ControlsProps = {
+  mode?: TMode;
+  setMode: React.Dispatch<React.SetStateAction<TMode | undefined>>;
+};
+
+export const Controls = ({ mode, setMode }: ControlsProps): JSX.Element => {
+  const handleClickButton = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    const currentMode = event.currentTarget.dataset.mode as TMode;
+    setMode((prev) => (prev === currentMode ? undefined : currentMode));
+  };
+
   return (
     <div className={styled.controls}>
       <Container className={styled.controls__container}>
-        <Button>Начать</Button>
-        <Button>Сбросить</Button>
-        <Button>Начать</Button>
+        <Button>Построить маршрут</Button>
+        <Button
+          data-mode="blocked"
+          active={mode === "blocked"}
+          onClick={handleClickButton}
+        >
+          Установить препятствия
+        </Button>
+        <Button
+          data-mode="points"
+          active={mode === "points"}
+          onClick={handleClickButton}
+        >
+          Установить начальную и конечную точки
+        </Button>
       </Container>
     </div>
   );
