1919c13c1fcf529bd9e39eb71ebfb3e2cb8b14a8
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-19 11:06:13 +0300

    refactor: add implementation of active button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,28 @@
+import cn from "classNames";
+
 import type { ButtonHTMLAttributes } from "react";
 
 import styled from "./Button.module.scss";
 
 type ButtonProps = Pick<
   ButtonHTMLAttributes<HTMLButtonElement>,
-  "disabled" | "type" | "children" | "className"
->;
+  "disabled" | "type" | "children" | "className" | "onClick"
+> & {
+  active?: boolean;
+};
 
-export const Button = ({ children, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({
+  children,
+  active,
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
-    <button className={styled.button} {...props}>
+    <button
+      className={cn(styled.button, props.className, {
+        [styled.active]: active,
+      })}
+      {...props}
+    >
       {children}
     </button>
   );
