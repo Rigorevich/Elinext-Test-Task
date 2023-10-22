053161c0fb4d47c81fe5a531d3282c33eca6ecc8
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-22 18:39:44 +0300

    refactor: display controls & extra info

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,48 +1,26 @@
 import { Container } from "../Container";
 import { Button } from "../Button";
-import type { TMode } from "../../types";
+import { useGrid, useParams } from "../../hooks";
 
 import styled from "./Controls.module.scss";
 
-type ControlsProps = {
-  mode?: TMode;
-  setMode: React.Dispatch<React.SetStateAction<TMode | undefined>>;
-  handleClearClick: () => void;
-  handleRouteClick: () => void;
-};
-
-export const Controls = ({
-  mode,
-  setMode,
-  handleClearClick,
-  handleRouteClick,
-}: ControlsProps): JSX.Element => {
-  const handleClickButton = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ): void => {
-    const currentMode = event.currentTarget.dataset.mode as TMode;
-    setMode((prev) => (prev === currentMode ? undefined : currentMode));
-  };
+export const Controls = (): JSX.Element => {
+  const { findPath, resetGrid } = useGrid();
+  const { time, isVisualized } = useParams();
 
   return (
     <div className={styled.controls}>
       <Container className={styled.controls__container}>
-        <Button onClick={handleRouteClick}>Построить маршрут</Button>
         <Button
-          data-mode="blocked"
-          active={mode === "blocked"}
-          onClick={handleClickButton}
+          typeStyle="secondary"
+          onClick={findPath}
+          disabled={isVisualized || typeof time === "number"}
         >
-          Установить препятствия
+          Построить маршрут
         </Button>
-        <Button
-          data-mode="points"
-          active={mode === "points"}
-          onClick={handleClickButton}
-        >
-          Установить начальную и конечную точки
+        <Button onClick={resetGrid} disabled={isVisualized}>
+          Сбросить
         </Button>
-        <Button onClick={handleClearClick}>Сбросить</Button>
       </Container>
     </div>
   );
--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,15 +1,21 @@
+import { memo } from "react";
+
+import { useParams } from "../../hooks/useParams";
 import { Container } from "../Container";
 
 import styled from "./Display.module.scss";
 
-export const Display = (): JSX.Element => {
+export const Display = memo((): JSX.Element => {
+  const { time, error } = useParams();
+
   return (
     <div className={styled.display}>
       <Container className={styled.display__container}>
-        <div className={styled.display__time}>Время выполнения: </div>
-        <div className={styled.display__error}>Ошибка: </div>
-        <div className={styled.display__hint}>Подсказка: </div>
+        <div className={styled.display__time}>
+          Время выполнения: {typeof time === "number" ? `${time} мс` : ""}
+        </div>
+        <div className={styled.display__error}>Ошибка: {error}</div>
       </Container>
     </div>
   );
-};
+});
