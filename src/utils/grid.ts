3d43a73fa0b65d12517e465fd74c2b31801c9673
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-22 16:57:14 +0300

    feat: add getShortestPath function

--- src/utils/grid.ts
+++ src/utils/grid.ts
@@ -1,17 +1,29 @@
 import { GRID_SIZE } from "../constants";
-import { TGrid, EGridCellType } from "../types/grid";
+import { TGrid, EGridCellType, TGridKey } from "../types";
 
 export const gridCompletion = () => {
   const initialGridState: TGrid = {};
 
   for (let rowIndex = 0; rowIndex < GRID_SIZE; rowIndex++) {
     for (let columnIndex = 0; columnIndex < GRID_SIZE; columnIndex++) {
-      const cellKey = `${rowIndex}-${columnIndex}`;
-      initialGridState[cellKey] = {
-        type: EGridCellType.None,
+      const key: TGridKey = `${rowIndex}-${columnIndex}`;
+      initialGridState[key] = {
+        type: EGridCellType.Default,
       };
     }
   }
 
   return initialGridState;
+};
+
+export const getShortestPath = (grid: TGrid, endKey: TGridKey): TGridKey[] => {
+  const keyCells: TGridKey[] = [];
+  let previousCell: TGridKey | undefined = grid[endKey].previousCell;
+
+  while (previousCell) {
+    keyCells.push(previousCell);
+    previousCell = grid[previousCell].previousCell;
+  }
+
+  return keyCells;
 };
