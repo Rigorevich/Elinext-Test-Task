aca85950797c23d1b5e524e91c8abc530e9171c1
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-22 16:58:27 +0300

    refactor: implement typeStyles in Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,38 @@
-import cn from "classNames";
+import cn from "classnames";
+import { memo } from "react";
 
 import type { ButtonHTMLAttributes } from "react";
 
 import styled from "./Button.module.scss";
 
-type ButtonProps = Pick<
+type NativeButton = Pick<
   ButtonHTMLAttributes<HTMLButtonElement>,
-  "disabled" | "type" | "children" | "className" | "onClick"
-> & {
+  "disabled" | "children" | "className" | "onClick"
+>;
+
+type ButtonProps = NativeButton & {
   active?: boolean;
+  typeStyle?: "primary" | "secondary";
 };
 
-export const Button = ({
-  children,
-  active,
-  ...props
-}: ButtonProps): JSX.Element => {
-  return (
-    <button
-      className={cn(styled.button, props.className, {
-        [styled.active]: active,
-      })}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = memo(
+  ({
+    children,
+    active,
+    typeStyle = "primary",
+    ...props
+  }: ButtonProps): JSX.Element => {
+    return (
+      <button
+        className={cn(styled.button, props.className, {
+          [styled.active]: active,
+          [styled.primary]: typeStyle === "primary",
+          [styled.secondary]: typeStyle === "secondary",
+        })}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
