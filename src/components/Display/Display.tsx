37c7d8c41de9de138413046d6d5a3db997993762
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-23 21:46:49 +0300

    refactor: Display styles & Display logic

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,20 +1,25 @@
 import { memo } from "react";
 
-import { useParams } from "../../hooks/useParams";
 import { Container } from "../Container";
 
 import styled from "./Display.module.scss";
+import { useAppSelector } from "../../store/hooks";
 
 export const Display = memo((): JSX.Element => {
-  const { time, error } = useParams();
+  const time = useAppSelector((state) => state.grid.time);
+  const error = useAppSelector((state) => state.grid.error);
 
   return (
     <div className={styled.display}>
       <Container className={styled.display__container}>
-        <div className={styled.display__time}>
-          Время выполнения: {typeof time === "number" ? `${time} мс` : ""}
+        <div className={styled.display__wrapper}>
+          <div className={styled.display__time}>
+            Время выполнения: {time && `${time} мс`}
+          </div>
+          <div className={styled.display__error}>
+            Ошибка: <span>{error}</span>
+          </div>
         </div>
-        <div className={styled.display__error}>Ошибка: {error}</div>
       </Container>
     </div>
   );
