7aab7d4e7044e5f652e9ac60a8217733c8eea4b8
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-23 21:24:04 +0300

    refactor: moved from Context to Redux

--- src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./useGrid";
-export * from "./useParams";
--- src/hooks/useParams.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-import { GridContext, TGridContext } from "../context/GridContext";
-
-export const useParams = (): TGridContext => {
-  return useContext(GridContext) as TGridContext;
-};
