9f79b3678e053c44bd9381f22a26183aaf055031
Rigorevich Ilya <rigorevich268@gmail.com>
2023-10-23 21:34:24 +0300

    refactor: clear code after deleting Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Controls, Display, Grid } from "./components";
-import { GridProvider } from "./context/GridContext";
 
 export const App = (): JSX.Element => {
   return (
-    <GridProvider>
+    <>
       <Controls />
       <Display />
       <Grid />
-    </GridProvider>
+    </>
   );
 };
